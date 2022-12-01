d69df3979b0e10279ae8254defa5037bad1f8bbc
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-12-01 14:36:01 +0200

    Designing the add Category form

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,10 +17,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPen, faTrash, faCamera } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faPen, faTrash);
+library.add(faPen, faTrash, faCamera);
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
