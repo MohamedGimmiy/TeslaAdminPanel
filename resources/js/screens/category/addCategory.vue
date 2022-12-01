34b7d21a9cc3fe767e991e5d279c36ddba98f3b2
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-12-01 15:02:28 +0200

    Connecting the category form with the state of the component

--- resources/js/screens/category/addCategory.vue
+++ resources/js/screens/category/addCategory.vue
@@ -4,13 +4,13 @@
             <v-col offset-md="1" cols="10" class="mt-4">
 
                 <v-form ref="form">
-                    <v-text-field :counter="35" label="Name" required dark></v-text-field>
+                    <v-text-field v-model="name" :counter="35" label="Name" required dark></v-text-field>
 
-                    <v-text-field label="Order" required dark></v-text-field>
+                    <v-text-field v-model="order" label="Order" required dark></v-text-field>
 
                     <v-row class="mb-2">
                         <font-awesome-icon icon="camera" class="white--text mt-4 ml-2" style="font-size: 30px; margin-right: -20px;"></font-awesome-icon>
-                        <v-file-input label="Image" truncate-length="15" dark></v-file-input>
+                        <v-file-input v-model="image" label="Image" truncate-length="15" dark></v-file-input>
                     </v-row>
 
                     <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
@@ -25,39 +25,14 @@
 
 <script>
 export default {
-    data: () => ({
-        valid: true,
-        name: '',
-        nameRules: [
-            v => !!v || 'Name is required',
-            v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-        ],
-        email: '',
-        emailRules: [
-            v => !!v || 'E-mail is required',
-            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-        ],
-        select: null,
-        items: [
-            'Item 1',
-            'Item 2',
-            'Item 3',
-            'Item 4',
-        ],
-        checkbox: false,
-    }),
-
-    methods: {
-        validate() {
-            this.$refs.form.validate()
-        },
-        reset() {
-            this.$refs.form.reset()
-        },
-        resetValidation() {
-            this.$refs.form.resetValidation()
-        },
+    data: function () {
+        return {
+            name: '',
+            order: null,
+            image: ''
+        }
     },
+
 }
 </script>
 
